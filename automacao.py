8fbc56846422bac0d42076adeef78e1948fe6fa1
Gabriel Xavier de Gouveia Osório <xavierg814@gmail.com>
2022-10-24 11:20:50 -0300

    tratamento de excessões

--- automacao.py
+++ automacao.py
@@ -1,3 +1,4 @@
+from email import message
 import pandas as pd
 import pathlib  # verificação de extensão
 ### imports para a automação ###
@@ -10,14 +11,19 @@ import time
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from selenium.common.exceptions import *
 
 ### Transforma em .exe ###
 
 ### Inicia o navegador ###
+try:
+    chrome = webdriver.Chrome("chromedriver.exe")
+    url_abrir = 'https://forms.gle/biofPp1SwfdThVrc9'
+    chrome.get(url_abrir)
+except SessionNotCreatedException as erro:
+    print("Versão do Drivre está incorreta, favor atualizar driver dentro da pasta da automação")
+    messagebox.showerror("Erro", "Versão do chromedrive é incompativél com a versão atual do chrome, favor verificar sua ver~sao do chrome e atualize o chromedriver")
 
-chrome = webdriver.Chrome("Versão para teste/chromedriver.exe")
-url_abrir = 'https://forms.gle/biofPp1SwfdThVrc9'
-chrome.get(url_abrir)
 
 
 ### Try para caso de erro ###
@@ -33,20 +39,37 @@ def selecionar_arquivo():
 ### parte da Automacação###
 
 def iniciar_aut():
+    try:
+        df = pd.read_excel(arquivo)
+        df.dropna(subset=['PIS'], inplace=True)
 
-    df = pd.read_excel(arquivo)
-    df.dropna(subset=['PIS'], inplace=True)
+        for index, row in df.iterrows():
+            time.sleep(2)
 
-    for index, column in df.iterrows():
-        time.sleep(5)
+            elemento_PIS = chrome.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
+            elemento_botao = chrome.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')  # Elemento que seleciona o XPATH do botão
 
-        elemento_PIS = chrome.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
-        elemento_botao = chrome.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')  # Elemento que seleciona o XPATH do botão
+            elemento_PIS.clear()
+            time.sleep(1)
+            elemento_PIS.send_keys(row["PIS"])
+            elemento_botao.click()
 
-        elemento_PIS.send_keys(column["PIS"])
-        elemento_botao.click()
+            time.sleep(3)
+            chrome.execute_script("window.history.go(-1)")
+        messagebox.showinfo("Sucesso", "Todos os PIS da coluna foram enviados")
 
-        chrome.execute_script("window.history.go(-1)")
+    except NoSuchElementException as erro:
+        print("Xpath não encontrado, verifique se está na tela correta ou relate o problema ao dev", erro)
+        messagebox.showerror("Erro", "Falha ao Encontrar o Xpath", erro)
+    except KeyError as erro:
+        print("Coluna PIS não encontrada, favor verificar nome da coluna no arquivo excel", erro)
+        messagebox.showerror("Erro", "Coluna PIS não encontrada, favor verificar planilha selecionada")
+    except NoSuchWindowException as erro:
+        print("Janela alvo da automação foi fechada, favor reinicie o programa", erro)
+        messagebox.showerror("Erro", "Janela Alvo foi fehcada, favor reinicie a automação")
+    except Exception as erro:
+        print("Um erro Inesperado ocorreu, favor entrar em contato com o dev", erro)
+        messagebox.showerror("Erro", f"Um Erro inesperado ocorreu, favor entrar em contato com o dev: {erro}")
 
 ### interface gráfica ###
 
@@ -57,8 +80,9 @@ janela_aut.geometry("663x220+610+153")
 janela_aut.resizable(width=1, height=1)
 
 # Import imagens
-img_fundo = PhotoImage(file='Versão para teste/img_fundo2.png')
-bot_fund = PhotoImage(file='Versão para teste/bot_fundo.png')
+img_fundo = PhotoImage(file='img_fundo2.png')
+bot_fund = PhotoImage(file='bot_fundo.png')
+bot_iniciar = PhotoImage(file='bot_iniciar.png')
 
 # Labels
 lab_fundo = Label(janela_aut, image=img_fundo)
